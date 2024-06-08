f5ab35146a7d72e9ad4288b1cd12cc649674b210
Михаил <mixaila17may2003@gmail.com>
2024-06-08 13:11:12 +0400

    тест коммит

---
+++ Assets/Scripts/Enumerations/Abilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Enumeration
+{
+    public enum Abilities
+    {
+        [Description("Летающее существо")]
+        Fly = 0,
+        [Description("Стрелок")]
+        Archer = 1,
+        [Description("Двойной удар")]
+        DoubleAttack = 2,
+
+
+    }
+}
