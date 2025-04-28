28c3cbba87a8f387e894d3a5919d56f0af695273
Михаил <mixaila17may2003@gmail.com>
2025-04-28 22:27:51 +0400

    Добавил адаптацию UI шкалы поб мобилку

---
+++ Assets/Scripts/UI/Battle/Extensions/GridCellSizeAdaptiveScaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Battle.Extensions
+{
+    [RequireComponent(typeof(GridLayoutGroup))]
+    public class GridCellSizeAdaptiveScaler : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _container;
+
+        private GridLayoutGroup _grid;
+
+        private void Awake()
+        {
+            _grid = GetComponent<GridLayoutGroup>();
+        }
+
+        private void OnRectTransformDimensionsChange()
+        {
+            if (_container != null && _grid != null)
+            {
+                _grid.cellSize = new Vector2(_container.rect.width, _container.rect.width);
+            }
+        }
+    }
+
+}
